78c839c1cb7b001edc9752f7c0166328b93a0ed0
ildarjan <nobap84@rambler.ru>
2020-10-05 23:35:05 +0300

    replace header to Header component. Fix src path.

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Header from "../header/header";
 
 const MainPage = (props) => {
   const {offersCount} = props;
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header/>
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
@@ -94,7 +73,7 @@ const MainPage = (props) => {
                   </div>
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200"
+                      <img className="place-card__image" src="/img/apartment-01.jpg" width="260" height="200"
                         alt="Place image"/>
                     </a>
                   </div>
@@ -127,7 +106,7 @@ const MainPage = (props) => {
                 <article className="cities__place-card place-card">
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image"/>
+                      <img className="place-card__image" src="/img/room.jpg" width="260" height="200" alt="Place image"/>
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -160,7 +139,7 @@ const MainPage = (props) => {
                 <article className="cities__place-card place-card">
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200"
+                      <img className="place-card__image" src="/img/apartment-02.jpg" width="260" height="200"
                         alt="Place image"/>
                     </a>
                   </div>
@@ -196,7 +175,7 @@ const MainPage = (props) => {
                   </div>
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200"
+                      <img className="place-card__image" src="/img/apartment-03.jpg" width="260" height="200"
                         alt="Place image"/>
                     </a>
                   </div>
@@ -229,7 +208,7 @@ const MainPage = (props) => {
                 <article className="cities__place-card place-card">
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image"/>
+                      <img className="place-card__image" src="/img/room.jpg" width="260" height="200" alt="Place image"/>
                     </a>
                   </div>
                   <div className="place-card__info">
