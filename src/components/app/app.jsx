0c9bd63468c0f5409be16f9ffc21882fa0992d1a
ildarjan <nobap84@rambler.ru>
2020-10-05 23:35:25 +0300

    Add routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import PropTypes from "prop-types";
 import MainPage from '../main-page/main-page';
+import Login from "../login/login";
+import Favorites from "../favorites/favorite";
+import Offer from "../offer/offer";
 
 const App = (props) => {
   const {offersCount} = props;
   return (
-    <MainPage offersCount={offersCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <MainPage offersCount={offersCount}/>
+        </Route>
+        <Route exact path='/login'>
+          <Login/>
+        </Route>
+        <Route exact path='/favorites'>
+          <Favorites/>
+        </Route>
+        <Route exact path='/offer/:id'>
+          <Offer/>
+        </Route>
+        <Route render={() => (
+          <Fragment>
+            <h1>Page not Found</h1>
+            <br/>
+            <Link to='/'>Go to main page</Link>
+          </Fragment>
+        )}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
