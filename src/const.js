f57c7ddb79c8e6dd55fa67cfb112cee0d0ad8e10
ildarjan <nobap84@rambler.ru>
2020-10-14 21:12:39 +0300

    add Enums and Const's

--- src/const.js
+++ src/const.js
@@ -1,3 +1,28 @@
 export const Settings = {
-  OFFERS_COUNT: 969
+  OFFERS_COUNT: 969,
+  OFFERS_IMAGES_DISPLAY_LIMIT: 6,
+  NEIGHBOURHOOD_OFFERS_DISPLAY_LIMIT: 3,
 };
+
+export const OFFER_TYPES = [
+  `apartment`,
+  `room`,
+  `house`,
+  `hotel`,
+];
+
+export const OfferType = {
+  apartment: `Apartment`,
+  room: `Private Room`,
+  house: `House`,
+  hotel: `Hotel`,
+};
+
+export const RatingTitle = {
+  1: `terribly`,
+  2: `badly`,
+  3: `not bad`,
+  4: `good`,
+  5: `perfect`
+};
+
