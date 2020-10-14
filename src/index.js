bec3c1720c334bb8170cef627b031dc83cf81165
ildarjan <nobap84@rambler.ru>
2020-10-14 21:11:22 +0300

    add mocks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from './components/app/app';
+import React from "react";
+import ReactDOM from "react-dom";
+import {App} from "./components/app/app";
 import {Settings} from "./const";
+import {getOffer} from "./mocks/offer";
+import {getReview} from "./mocks/review";
+import {
+  MOCK_OFFERS_COUNT,
+  MOCK_REVIEWS_COUNT,
+} from "./mocks/consts";
 
 const root = document.querySelector(`#root`);
 
+const offers = Array(MOCK_OFFERS_COUNT).fill(``).map(getOffer);
+const reviews = Array(MOCK_REVIEWS_COUNT).fill(``).map(getReview);
+
 ReactDOM.render(
-    <App offersCount={Settings.OFFERS_COUNT}/>,
+    <App
+      offersCount={Settings.OFFERS_COUNT}
+      offers={offers}
+      reviews={reviews}
+    />,
     root
 );
