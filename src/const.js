1e2197429beea5a8261e7b7b20f422403ba9b269
ildarjan <nobap84@rambler.ru>
2020-10-21 19:45:50 +0300

    renaming

--- src/const.js
+++ src/const.js
@@ -1,11 +1,12 @@
+export const RATING_STARS_COUNT = 5;
 export const Settings = {
   OFFERS_COUNT: 969,
   OFFERS_IMAGES_DISPLAY_LIMIT: 6,
-  NEIGHBOURHOOD_OFFERS_DISPLAY_LIMIT: 3,
+  NEAR_OFFERS_DISPLAY_LIMIT: 3,
   REVIEW_TEXT_MIN_LENGTH: 50
 };
 
-export const OFFER_TYPES = [
+export const offerTypes = [
   `apartment`,
   `room`,
   `house`,
@@ -13,17 +14,26 @@ export const OFFER_TYPES = [
 ];
 
 export const OfferType = {
-  apartment: `Apartment`,
-  room: `Private Room`,
-  house: `House`,
-  hotel: `Hotel`,
+  APARTMENT: `Apartment`,
+  ROOM: `Private Room`,
+  HOUSE: `House`,
+  HOTEL: `Hotel`,
 };
 
 export const RatingTitle = {
-  1: `terribly`,
-  2: `badly`,
-  3: `not bad`,
-  4: `good`,
-  5: `perfect`
+  PERFECT: 5,
+  GOOD: 4,
+  NOT_BAD: 3,
+  BADLY: 2,
+  TERRIBLY: 1
 };
+
+export const Title = {
+  PERFECT: `perfect`,
+  GOOD: `good`,
+  NOT_BAD: `not bad`,
+  BADLY: `badly`,
+  TERRIBLY: `terribly`
+};
+
 
