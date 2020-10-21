78460225f149c642cb206702aacb534ee2c8f714
ildarjan <nobap84@rambler.ru>
2020-10-21 19:51:35 +0300

    remove destruction. refactor.

--- src/components/property-review/property-review.jsx
+++ src/components/property-review/property-review.jsx
@@ -3,40 +3,32 @@ import {reviewPropTypes} from "../../prop-validation/review-prop-types";
 import {
   getReviewDateString,
   getReviewTimeDateString,
+  getRatingWidth
 } from "../../utils";
 
-export const PropertyReview = ({review: singleReview}) => {
-  const {
-    avatar,
-    name,
-    date,
-    rating,
-    review,
-  } = singleReview;
-  const ratingToSpanWidth = Math.round(100 / 5 * rating);
-
+export const PropertyReview = ({review}) => {
   return (
     <li className="reviews__item">
       <div className="reviews__user user">
         <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src={avatar} width="54" height="54"
-            alt={`${name} avatar`}/>
+          <img className="reviews__avatar user__avatar" src={review.avatar} width="54" height="54"
+            alt={`${review.name} avatar`}/>
         </div>
-        <span className="reviews__user-name">{name}</span>
+        <span className="reviews__user-name">{review.name}</span>
       </div>
       <div className="reviews__info">
         <div className="reviews__rating rating">
           <div className="reviews__stars rating__stars">
-            <span style={{width: `${ratingToSpanWidth}%`}}/>
+            <span style={{width: `${getRatingWidth(review.rating)}%`}}/>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
-        <p className="reviews__text">{review}</p>
+        <p className="reviews__text">{review.review}</p>
         <time
           className="reviews__time"
-          dateTime={getReviewTimeDateString(date)}
+          dateTime={getReviewTimeDateString(review.date)}
         >
-          {getReviewDateString(date)}
+          {getReviewDateString(review.date)}
         </time>
       </div>
     </li>
