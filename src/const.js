293eea338aa63ee119fa65e397def6ba5551c695
ildarjan <nobap84@rambler.ru>
2020-10-22 18:56:37 +0300

    add city coords and cities

--- src/const.js
+++ src/const.js
@@ -1,9 +1,9 @@
-export const RATING_STARS_COUNT = 5;
 export const Settings = {
   OFFERS_COUNT: 969,
   OFFERS_IMAGES_DISPLAY_LIMIT: 6,
   NEAR_OFFERS_DISPLAY_LIMIT: 3,
-  REVIEW_TEXT_MIN_LENGTH: 50
+  REVIEW_TEXT_MIN_LENGTH: 50,
+  RATING_STARS_COUNT: 5
 };
 
 export const offerTypes = [
@@ -37,3 +37,38 @@ export const Title = {
 };
 
 
+export const CityCoords = {
+  AMSTERDAM: {
+    lat: 52.38333,
+    lng: 4.9,
+  },
+  PARIS: {
+    lat: 48.866667,
+    lng: 2.333333,
+  },
+  COLOGNE: {
+    lat: 50.933333,
+    lng: 6.95,
+  },
+  BRUSSELS: {
+    lat: 50.833333,
+    lng: 4.333333,
+  },
+  HAMBURG: {
+    lat: 53.575323,
+    lng: 10.01534,
+  },
+  DUSSELDORF: {
+    lat: 51.228304,
+    lng: 6.793849,
+  },
+};
+
+export const cities = [
+  `AMSTERDAM`,
+  `PARIS`,
+  `COLOGNE`,
+  `BRUSSELS`,
+  `HAMBURG`,
+  `DUSSELDORF`
+];
