f58b62e869960b53e25589710e5449842db9b71c
ildarjans <nobap84@rambler.ru>
2020-11-10 15:06:36 +0300

    remove connection to store

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,41 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
-import {connect} from "react-redux";
-
-import {actionCreator} from "../../store/action";
-import {cities} from "../../const";
 
 import {Header} from "../header/header";
 import {Tabs} from "../tabs/tabs";
 import {Cities} from "../cities/cities";
 
-const MainPageComponent = ({activeCity, onCityChange}) => {
+export const MainPage = () => {
   return (
     <div className="page page--gray page--main">
       <Header/>
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
-        <Tabs activeCity={activeCity} onTabClick={onCityChange}/>
-        <Cities activeCity={activeCity}/>
+        <Tabs />
+        <Cities />
       </main>
     </div>
   );
 };
-
-MainPageComponent.propTypes = {
-  activeCity: PropTypes.oneOf(cities).isRequired,
-  onCityChange: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  activeCity: state.activeCity,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  onCityChange: (city) => {
-    dispatch(actionCreator.changeCity(city));
-  }
-});
-
-export const MainPage = connect(mapStateToProps, mapDispatchToProps)(MainPageComponent);
-
