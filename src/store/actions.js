24a5b8b0ee91b41ef0dcf69bfcfdb908974d1e13
ildarjans <nobap84@rambler.ru>
2020-11-17 18:07:59 +0300

    added actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,18 +1,16 @@
 export const ActionType = {
-  GET_OFFERS: `GET_OFFERS`,
   CHANGE_CITY: `CHANGE_CITY`,
   CHANGE_ACTIVE_OFFER_ID: `CHANGE_ACTIVE_OFFER_ID`,
-  CHANGE_SORT_TYPE: `CHANGE_SORT_TYPE`
+  CHANGE_SORT_TYPE: `CHANGE_SORT_TYPE`,
+  GET_HOTELS: `GET_HOTELS`,
+  GET_REVIEWS: `GET_REVIEWS`,
+  GET_NEAR_HOTELS: `GET_NEAR_HOTELS`,
 };
 
 export const actionCreator = {
   changeCity: (city) => ({
     type: ActionType.CHANGE_CITY,
     payload: city
-  }),
-  getOffers: (offers, city) => ({
-    type: ActionType.GET_OFFERS,
-    payload: offers.filter((offer) => offer.city === city)
   }),
   changeActiveOfferId: (id) => ({
     type: ActionType.CHANGE_ACTIVE_OFFER_ID,
@@ -21,5 +19,17 @@ export const actionCreator = {
   changeSortType: (sortType) => ({
     type: ActionType.CHANGE_SORT_TYPE,
     payload: sortType
+  }),
+  getHotels: (hotels) => ({
+    type: ActionType.GET_HOTELS,
+    payload: hotels,
+  }),
+  getHotelReviews: (reviews) => ({
+    type: ActionType.GET_REVIEWS,
+    payload: reviews
+  }),
+  getNearHotels: (hotels) => ({
+    type: ActionType.GET_NEAR_HOTELS,
+    payload: hotels
   })
 };
