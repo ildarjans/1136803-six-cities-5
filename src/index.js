29687e184275e44a7d5ef1a925696513fafdfda3
ildarjans <nobap84@rambler.ru>
2020-11-17 18:41:52 +0300

    apply thunk middleware. Async render App.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
 
-import {reducer} from "./store/reducer";
+import {createAPI} from "./services/api";
+import {rootReducer} from "./store/root-reducer";
+import {fetchHotels} from "./store/api-actions";
+
 import {App} from "./components/app/app";
 
+const api = createAPI();
+
 const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
-ReactDOM.render(
-    <Provider store={store}>
-      <App/>,
-    </Provider>,
-    document.querySelector(`#root`)
-);
+Promise.all([
+  store.dispatch(fetchHotels())
+])
+.then(() => {
+  ReactDOM.render(
+      <Provider store={store}>
+        <App/>,
+      </Provider>,
+      document.querySelector(`#root`)
+  );
+});
