9adbd7ad796957496a2d6f6bf8e8c6644754c923
ildarjans <nobap84@rambler.ru>
2020-11-27 22:33:18 +0300

    add start action to fetch favorite offers.

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ import thunk from "redux-thunk";
 
 import {createAPI} from "./services/api";
 import {rootReducer} from "./store/root-reducer";
-import {checkAuthStatus, fetchOffers} from "./middleware/thunk-api";
+import {
+  checkAuthStatus,
+  fetchFavoritesOffers,
+  fetchOffers
+} from "./middleware/thunk-api";
 
 import {App} from "./components/app/app";
 import {authActionCreator} from "./store/user/user-action";
 import {redirect} from "./middleware/redirect";
 
 const api = createAPI(() => {
-  store.dispatch(authActionCreator.requireAuthorizationStatus());
+  store.dispatch(authActionCreator.authorizationFail());
 });
 
 const store = createStore(
@@ -27,7 +31,8 @@ const store = createStore(
 
 Promise.all([
   store.dispatch(fetchOffers()),
-  store.dispatch(checkAuthStatus())
+  store.dispatch(checkAuthStatus()),
+  store.dispatch(fetchFavoritesOffers()),
 ])
 .then(() => {
   ReactDOM.render(
@@ -37,3 +42,4 @@ Promise.all([
       document.querySelector(`#root`)
   );
 });
+
