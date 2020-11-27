1154ebc0f4a5896c36b72d0f82b732a99a48a04d
ildarjans <nobap84@rambler.ru>
2020-11-27 22:39:09 +0300

    add empty-main component

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,18 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 
-import {Header} from "../header/header";
+import {selectCityOffers} from "../../selectors/selectors";
+import {offerPropTypes} from "../../prop-types/offer";
+
 import {LocationsTabs} from "../locations-tabs/locations-tabs";
+import {Header} from "../header/header";
 import {Cities} from "../cities/cities";
+import {MainEmpty} from "../empty-main/empty-main";
 
-export const MainPage = () => {
+const MainPageComponent = ({cityOffers}) => {
   return (
     <div className="page page--gray page--main">
       <Header/>
-      <main className="page__main page__main--index">
-        <h1 className="visually-hidden">Cities</h1>
-        <LocationsTabs />
-        <Cities />
-      </main>
+      {
+        cityOffers.length === 0 ?
+          <MainEmpty/> :
+          <main className="page__main page__main--index">
+            <h1 className="visually-hidden">Cities</h1>
+            <LocationsTabs/>
+            <Cities/>
+          </main>
+      }
     </div>
   );
 };
+
+MainPageComponent.propTypes = {
+  cityOffers: PropTypes.arrayOf(offerPropTypes).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  cityOffers: selectCityOffers(state),
+});
+
+export const MainPage = connect(mapStateToProps)(MainPageComponent);
